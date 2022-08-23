ad42e363bb4664d7ac42d4f7d0d4e63a7eb95b94
Tom Myers <tmyers273@gmail.com>
2022-08-23 20:21:08 +0100

    Initial Commit

--- interfaces.go
+++ interfaces.go
@@ -1,29 +1,29 @@
 package react
 
 type Reactor interface {
-			CreateInput(int) InputCell
+	CreateInput(int) InputCell
 
-				CreateCompute1(Cell, func(int) int) ComputeCell
+	CreateCompute1(Cell, func(int) int) ComputeCell
 
-				CreateCompute2(Cell, Cell, func(int, int) int) ComputeCell
+	CreateCompute2(Cell, Cell, func(int, int) int) ComputeCell
 }
 
 type Cell interface {
-		Value() int
+	Value() int
 }
 
 type InputCell interface {
 	Cell
 
-		SetValue(int)
+	SetValue(int)
 }
 
 type ComputeCell interface {
 	Cell
 
-			AddCallback(func(int)) Canceler
+	AddCallback(func(int)) Canceler
 }
 
 type Canceler interface {
-		Cancel()
+	Cancel()
 }
